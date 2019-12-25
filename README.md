5c1396fa369ef1750dfad174600a3789b22c9a16
Gabriel Cánepa <gacanepa@gmail.com>
2019-12-25 13:17:23 -0300

    Updated README with links, camelCase names for functions + one extra example for Chapter 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # javascript-exercises
-This repository is used to store the Javascript code found in the book "What you need to know about Javascript", written by me, reviewed by Walter Molina, and published by Packt Publishing.
+This repository is used to store the Javascript code found in the book [What you need to know about Javascript](https://1drv.ms/b/s!AotpOipZ2Sr2hhsT-3OSA2uuounE), written by [me](https://ar.linkedin.com/in/gacanepa), reviewed by [Walter Molina](https://www.linkedin.com/in/waltermolina/), and published by Packt Publishing.
 
 ### Author bio
 Gabriel A. Canepa is a [Linux Foundation certified system administrator](https://training.linuxfoundation.org/certification/verify-linux-certifications) (LFCS-1500-0576-0100), technical writer, and web developer from Villa Mercedes, San Luis, Argentina. He works for a software development business doing DevOps work and takes great pleasure in using FOSS tools to increase productivity in all areas of his daily work. When he's not typing commands or writing code or articles, he enjoys telling bedtime stories with his wife to his two little daughters and playing with them, which is a great pleasure of his life.
--- chapter1/js/test.js
+++ chapter1/js/test.js
@@ -1,19 +1,37 @@
-/* Function declaration
-   ====================
-SayHi requires 3 parameters: your name, age, and profession (in that order).
+/* Function declarations
+   =====================
+sayHi requires 3 parameters: your name, age, and profession (in that order).
 In the function definition, these 3 parameters are named a, b, and c.
 The function returns a greeting using those values.
+
+getDayOfWeek returns the current day of the week (Sunday = 0 through Saturday = 6)
+
 */
-function SayHi(a,b,c){
+function sayHi(a, b, c){
 	var person = {name: a, age: b, profession: c};
 	return 'My name is ' + person.name + ' and am ' + person.age + ' years old. I am a ' + person.profession + '.';
+}
+
+function getDayOfWeek() {
+    var d = new Date();
+    var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return "Happy " + weekDay[d.getDay()];
 }
 
 /* Object definition
    =================
 introducingMyself is an object variable that gets the first h3 element in the
 document. This element has a property called textContent where we will show
-the result of calling SayHi with the indicated parameters.
+the result of calling sayHi with the indicated parameters.
+
+It is important to note that textContent sets or returns the text content of the specified node 
+and all its descendants, whereas innerHTML property sets or returns the HTML content of an element.
+
+mainHeader stores the element with the ID of "header". We will use its innerHTML
+property to display the output of getDayOfWeek.
 */
-var introducingMyself = document.querySelector('h3');
-introducingMyself.textContent = SayHi('Gabriel', 33, 'programmer');
+var introducingMyself = document.querySelector('p'); // Document.querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+introducingMyself.textContent = sayHi('Gabriel', 37, 'programmer');
+
+var mainHeader = document.getElementById("header"); // Document.getElementById(): https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+mainHeader.innerHTML = getDayOfWeek();
