40ecd2812fbe5c23bc3fd57c5261f81f8ccb83b4
Gabriel Cánepa <gacanepa@gmail.com>
2019-12-28 14:33:47 -0300

    Minor fixes + added resources page

--- chapter4/examples.js
+++ chapter4/examples.js
@@ -1,14 +1,16 @@
 // Demonstration of Array.includes
 // Given the follow array
 var famousWriters = [];
-// Let's add a few items
+
+// Let's add a few items. Each operation returns the new length of the array.
 famousWriters.push("C. S. Lewis");
 famousWriters.push("J. K. Rowling");
 famousWriters.push("J. R. R. Tolkien");
 famousWriters.push("Richard Bach");
+
 /* Then let's check whether the indicated items are included in the array or not. 
    The syntax allows to specify not only the item to search for but also the index 
-   where the search should be started */
+   where the search should start */
 famousWriters.includes("C. S. Lewis"); //true
 famousWriters.includes("Richard Bach"); //true
 famousWriters.includes("C. S. Lewis", 1); //false
@@ -16,20 +18,20 @@ famousWriters.includes("John Doe"); //false
 
 // Demonstration of Object.values/Object.entries
 // Given the following object
-var person = { name: "Gabriel", age: 33, profession: "developer" };
+var person = { name: "Gabriel", age: 37, profession: "developer" };
+
 /* Let's return the values in each key/value pair. In this context,
    a key/value pair is also known as an entry. */
-console.log(Object.values(person)); //['Gabriel', 33, 'developer']
+console.log(Object.values(person)); //['Gabriel', 37, 'developer']
 // Let's return the each key/value pair, or entry.
 console.log(Object.entries(person)); //[ ['name', 'Gabriel'], ['age', 33], ['profession', 'developer'] ]
 
 // Demonstration of String padding
-console.log(1.padStart(10)); //"         1"
+console.log('1'.padStart(10)); //"         1"
 console.log('1'.padStart(10, "0")); //"0000000001"
 /* When the specified padding is less than or equal to the length 
    of the original string, no padding characters are added. */
 console.log('1'.padEnd(10)); //"1         "
 console.log('1'.padEnd(10, "0")); //"1000000000"
-console.log('2016'.padStart(4, "0")); //"2016"
-console.log('2016'.padEnd(3, "0")); //"2016"
-
+console.log('2019'.padStart(4, "0")); //"2016"
+console.log('2019'.padEnd(3, "0")); //"2016"
---
+++ chapter4/new-features.md
@@ -0,0 +1,8 @@
+# Useful resources
+
+- ES6 (2015) features
+  - [Github repository](https://github.com/rse/es6-features)
+  - [Website](http://es6-features.org)
+
+- Examples of ES2016, 2017, and 2018
+  - [Freecodecamp](https://www.freecodecamp.org/news/here-are-examples-of-everything-new-in-ecmascript-2016-2017-and-2018-d52fa3b5a70e/) article
