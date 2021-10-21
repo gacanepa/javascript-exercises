f3f5bc9ecbaa5ad35c1835131c50e2b3636e9da4
Gabriel Cánepa <gacanepa@gmail.com>
2021-10-21 12:20:03 -0300

    Replaced string concatenation with template literals

--- chapter1/js/test.js
+++ chapter1/js/test.js
@@ -1,37 +1,47 @@
 /* Function declarations
    =====================
-sayHi requires 3 parameters: your name, age, and profession (in that order).
-In the function definition, these 3 parameters are named a, b, and c.
+sayHi requires 3 parameters: your name, age, and profession (a default value for the latter is provided)
 The function returns a greeting using those values.
-
 getDayOfWeek returns the current day of the week (Sunday = 0 through Saturday = 6)
 
 */
-function sayHi(a, b, c){
-	var person = {name: a, age: b, profession: c};
-	return 'My name is ' + person.name + ' and am ' + person.age + ' years old. I am a ' + person.profession + '.';
+const sayHi = props => {
+  const { name, age, profession } = props;
+  return `My name is ${name} and I am a ${age}-year-old ${profession}.`;
 }
 
 function getDayOfWeek() {
-    var d = new Date();
-    var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return "Happy " + weekDay[d.getDay()];
+  const today = new Date();
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return `Happy ${weekDays[today.getDay()]}`;
 }
 
 /* Object definition
    =================
-introducingMyself is an object variable that gets the first h3 element in the
+introducingMyself is an object variable that gets the first p element in the
 document. This element has a property called textContent where we will show
 the result of calling sayHi with the indicated parameters.
 
 It is important to note that textContent sets or returns the text content of the specified node 
-and all its descendants, whereas innerHTML property sets or returns the HTML content of an element.
+and all its descendants, whereas innerHTML property sets or returns the HTML content of an element. Thus,
+textContent has better performance because its value is not parsed as HTML.
 
-mainHeader stores the element with the ID of "header". We will use its innerHTML
-property to display the output of getDayOfWeek.
+mainHeader stores the element with the ID of "header"
 */
-var introducingMyself = document.querySelector('p'); // Document.querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
-introducingMyself.textContent = sayHi('Gabriel', 37, 'programmer');
+const introducingMyself = document.querySelector('p'); // Document.querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+introducingMyself.textContent = sayHi({ 
+   name: 'Gabriel',
+   age: 39,
+   profession: 'software developer'
+});
 
-var mainHeader = document.getElementById("header"); // Document.getElementById(): https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-mainHeader.innerHTML = getDayOfWeek();
+const mainHeader = document.getElementById('header'); // Document.getElementById(): https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+mainHeader.textContent = getDayOfWeek();
